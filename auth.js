38e0ee9c3ed09c1ca452be072147713122d2b430
Hong Joon <hongjoon@Hongs-MacBook-Air.local>
2016-11-10 23:34:53 +0800

    [DEV] changed oAuth callbackURL

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 module.exports = {
   "clientId": "156408871493935",
   "clientSecret": "6cfe097481b48981cabe396f0a93a0d8",
-  "callbackURL": "http://localhost8080/auth/facebook/callback"
+  "callbackURL": "http://yournote.herokuapp.com/auth/facebook/callback"
 }
